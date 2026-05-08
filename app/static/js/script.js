4d52ecee03ca4210e156244615eb1af329bece85
Jessica John Osta <jessica.john@btech.christuniversity.in>
2026-05-08 10:37:14 +0530

    Added forecast feature and weather API endpoint. But the use my location is not working properly, tho it is fetching the location and the latitudes and logitudes but it not being able to fetch the city and show the details, need to work on that feature

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,3 +6,23 @@ form.addEventListener("submit", () => {
 
     loading.classList.remove("hidden");
 });
+const locationBtn = document.getElementById("locationBtn");
+
+locationBtn.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+            async(position) => {
+
+                const lat = position.coords.latitude;
+
+                const lon = position.coords.longitude;
+
+                alert(
+                    `Latitude: ${lat}, Longitude: ${lon}`
+                );
+            }
+        );
+    }
+});
